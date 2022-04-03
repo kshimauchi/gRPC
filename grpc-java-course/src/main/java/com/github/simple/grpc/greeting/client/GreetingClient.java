859c549d93a1460f5169c648d8b352b1772d255f
unknown <kshimauchi@gmail.com>
2022-04-03 11:13:34 -0800

    grpc created basic client for unary example

--- grpc-java-course/src/main/java/com/github/simple/grpc/greeting/client/GreetingClient.java
+++ grpc-java-course/src/main/java/com/github/simple/grpc/greeting/client/GreetingClient.java
@@ -1,6 +1,10 @@
 package com.github.simple.grpc.greeting.client;
 
 import com.proto.dummy.DummyServiceGrpc;
+import com.proto.greet.GreetRequest;
+import com.proto.greet.GreetResponse;
+import com.proto.greet.GreetServiceGrpc;
+import com.proto.greet.Greeting;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 
@@ -15,7 +19,28 @@ public class GreetingClient {
               .build();
     System.out.println("Creating Stub");
 
-    DummyServiceGrpc.DummyServiceBlockingStub syncClient = DummyServiceGrpc.newBlockingStub(channel);
+    //DummyServiceGrpc.DummyServiceBlockingStub syncClient = DummyServiceGrpc.newBlockingStub(channel);
+
+    GreetServiceGrpc.GreetServiceBlockingStub greetClient = GreetServiceGrpc.newBlockingStub(channel);
+
+    //Appears to look like your calling a method directly but
+    // this is actually going over the network rpc
+
+    // created a protocol buffer greeting message
+    Greeting greeting = Greeting.newBuilder()
+                    .setFirstName("Ken")
+                            .setLastName("Shimauchi")
+                                    .build();
+    // Create a greetRequest and set the message
+    GreetRequest greetRequest = GreetRequest.newBuilder()
+                    .setGreeting(greeting)
+                            .build();
+    // Create a greetResponse and pass the greetClient a greetRequest
+    GreetResponse greetResponse = greetClient.greet(greetRequest);
+
+
+    // Print response
+    System.out.println(greetResponse.getResult());
 
     System.out.println("Shutting down channel");
 
