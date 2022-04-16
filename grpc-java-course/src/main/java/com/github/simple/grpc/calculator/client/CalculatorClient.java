80fc6267279f03726e947c69eaf9c29f4dd25486
unknown <kshimauchi@gmail.com>
2022-04-15 21:13:52 -0800

    client streaming example

--- grpc-java-course/src/main/java/com/github/simple/grpc/calculator/client/CalculatorClient.java
+++ grpc-java-course/src/main/java/com/github/simple/grpc/calculator/client/CalculatorClient.java
@@ -1,43 +1,115 @@
 package com.github.simple.grpc.calculator.client;
 
-import com.proto.calculator.CalculatorServiceGrpc;
-import com.proto.calculator.PrimeNumberDecompositionRequest;
-import com.proto.calculator.SumRequest;
-import com.proto.calculator.SumResponse;
+import com.github.simple.grpc.greeting.client.GreetingClient;
+import com.proto.calculator.*;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
+import io.grpc.stub.StreamObserver;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 
 public class CalculatorClient {
-    public static void main(String [] args){
+    private void run(){
         // Create a channel on given domain and port we will later be able to use ssl
         ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost",50052)
                 .usePlaintext()
                 .build();
+        //doUnaryCall(channel);
+        //doServerStreamingCall(channel);
+        //doClientStreamCall(channel);
+        doClientStreamCall(channel);
 
+        System.out.println("Shutting down channel ");
+
+        channel.shutdown();
+    }
+    
+    private void doUnaryCall(ManagedChannel channel){
         // Create the stub for the CalculatorServiceGrpc
         CalculatorServiceGrpc.CalculatorServiceBlockingStub stub = CalculatorServiceGrpc.newBlockingStub(channel);
-// Unary
-//        // Create a request for the Calculator Service
-//        SumRequest request = SumRequest.newBuilder()
-//                .setFirstNumber(10)
-//                .setSecondNumber(20)
-//                .build();
-//
-//        SumResponse response = stub.sum(request);
-//
-//        System.out.println(request.getFirstNumber() + " + " + request.getSecondNumber() + " = " + response.getSumResult() );
+        //Unary
+        // Create a request for the Calculator Service
+        SumRequest request = SumRequest.newBuilder()
+                .setFirstNumber(10)
+                .setSecondNumber(20)
+                .build();
+
+        SumResponse response = stub.sum(request);
+
+        System.out.println(request.getFirstNumber() + " + " + request.getSecondNumber() + " = " + response.getSumResult() );
+
+    }
+
+    private void doServerStreamingCall(ManagedChannel channel){
+        // Create the stub for the CalculatorServiceGrpc
+        CalculatorServiceGrpc.CalculatorServiceBlockingStub stub = CalculatorServiceGrpc.newBlockingStub(channel);
 
         // Streaming Server
         Long number = 5678901285453333201L;
         stub.primeNumberDecomposition(
-            PrimeNumberDecompositionRequest.newBuilder().setNumber(number).build())
-        .forEachRemaining(
-            primeNumberDecompositionResponse -> {
-              System.out.println(primeNumberDecompositionResponse.getPrimeFactor());
-            });
+                        PrimeNumberDecompositionRequest.newBuilder().setNumber(number).build())
+                .forEachRemaining(
+                        primeNumberDecompositionResponse -> {
+                            System.out.println(primeNumberDecompositionResponse.getPrimeFactor());
+                        });
+    }
 
-        // Shut down the channel
-        channel.shutdown();
+    private void doClientStreamCall(ManagedChannel channel){
+        // Create the stub for the CalculatorServiceGrpc
+        CalculatorServiceGrpc.CalculatorServiceStub asyncClient = CalculatorServiceGrpc.newStub(channel);
+
+        CountDownLatch latch = new CountDownLatch(1);
+
+        StreamObserver<ComputeAverageRequest> requestObserver = asyncClient.computeAverage(new StreamObserver<ComputeAverageResponse>() {
+            @Override
+            public void onNext(ComputeAverageResponse value) {
+                System.out.println("Received Response from server ");
+                System.out.println(value.getAverage());
+            }
+
+            @Override
+            public void onError(Throwable t) {
+
+            }
+
+            @Override
+            public void onCompleted() {
+                System.out.println("Server has completed sending us data");
+                latch.countDown();
+            }
+        });
+
+        requestObserver.onNext(ComputeAverageRequest.newBuilder()
+                .setNumber(1)
+                .build());
+
+        requestObserver.onNext(ComputeAverageRequest.newBuilder()
+                .setNumber(2)
+                .build());
+        requestObserver.onNext(ComputeAverageRequest.newBuilder()
+                .setNumber(3)
+                .build());
+        requestObserver.onNext(ComputeAverageRequest.newBuilder()
+                .setNumber(4)
+                .build());
+        //expect the average 10 /4 = 2.5
+        requestObserver.onCompleted();
+
+        try {
+            latch.await(3, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String [] args){
+        System.out.println("Hello I am a grpc client");
+
+        CalculatorClient main = new CalculatorClient();
+        main.run();
+
+
     }
 }
--- grpc-java-course/src/main/java/com/github/simple/grpc/calculator/server/CalculatorServiceImpl.java
+++ grpc-java-course/src/main/java/com/github/simple/grpc/calculator/server/CalculatorServiceImpl.java
@@ -1,6 +1,9 @@
 package com.github.simple.grpc.calculator.server;
 
 import com.proto.calculator.*;
+import io.grpc.Status;
+import io.grpc.StatusException;
+import io.grpc.StatusRuntimeException;
 import io.grpc.stub.StreamObserver;
 
 public class CalculatorServiceImpl extends CalculatorServiceGrpc.CalculatorServiceImplBase {
@@ -30,5 +33,79 @@ public class CalculatorServiceImpl extends CalculatorServiceGrpc.CalculatorServi
             }
         }
         responseObserver.onCompleted();
+    }
+
+    @Override
+    public StreamObserver<ComputeAverageRequest> computeAverage(StreamObserver<ComputeAverageResponse> responseObserver) {
+
+        StreamObserver<ComputeAverageRequest> requestObserver = new StreamObserver<ComputeAverageRequest>() {
+            //running sum and count
+            int sum = 0;
+            int count = 0;
+
+
+            /**
+             * Receives a value from the stream.
+             *
+             * <p>Can be called many times but is never called after {@link #onError(Throwable)} or {@link
+             * #onCompleted()} are called.
+             *
+             * <p>Unary calls must invoke onNext at most once.  Clients may invoke onNext at most once for
+             * server streaming calls, but may receive many onNext callbacks.  Servers may invoke onNext at
+             * most once for client streaming calls, but may receive many onNext callbacks.
+             *
+             * <p>If an exception is thrown by an implementation the caller is expected to terminate the
+             * stream by calling {@link #onError(Throwable)} with the caught exception prior to
+             * propagating it.
+             *
+             * @param value the value passed to the stream
+             */
+            @Override
+            public void onNext(ComputeAverageRequest value) {
+                //increment the sum
+                sum += value.getNumber();
+                //increment the count
+                count++;
+            }
+
+            /**
+             * Receives a terminating error from the stream.
+             *
+             * <p>May only be called once and if called it must be the last method called. In particular if an
+             * exception is thrown by an implementation of {@code onError} no further calls to any method are
+             * allowed.
+             *
+             * <p>{@code t} should be a {@link StatusException} or {@link
+             * StatusRuntimeException}, but other {@code Throwable} types are possible. Callers should
+             * generally convert from a {@link Status} via {@link Status#asException()} or
+             * {@link Status#asRuntimeException()}. Implementations should generally convert to a
+             * {@code Status} via {@link Status#fromThrowable(Throwable)}.
+             *
+             * @param t the error occurred on the stream
+             */
+            @Override
+            public void onError(Throwable t) {
+
+            }
+
+            /**
+             * Receives a notification of successful stream completion.
+             *
+             * <p>May only be called once and if called it must be the last method called. In particular if an
+             * exception is thrown by an implementation of {@code onCompleted} no further calls to any method
+             * are allowed.
+             */
+            @Override
+            public void onCompleted() {
+                double average = (double) sum/count;
+                responseObserver.onNext(
+                        ComputeAverageResponse.newBuilder()
+                                .setAverage(average)
+                                .build()
+                );
+                responseObserver.onCompleted();
+            }
+        };
+        return requestObserver;
     }
 }
